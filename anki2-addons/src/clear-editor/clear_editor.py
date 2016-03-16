175acac5f1bb028b957d896da0c98e0d2174508a
Mirco Kraenz <prosingularity@yahoo.de>
2016-03-16 20:20:25 +0100

    #1 RENAME always use clear

---
+++ anki2-addons/src/clear-editor/clear_editor.py
@@ -0,0 +1,31 @@
+'''
+Created on 16.03.2016
+
+@author: Mirco Kraenz
+'''
+
+# Tested with Anki Version 2.0.33, Qt 4.8.4, PyQt 4.10
+
+# import the main window object (mw) from ankiqt
+from aqt import mw
+from aqt.editor import Editor # the editor when you click "Add" in Anki
+from anki.hooks import addHook
+
+SHORTCUT = "Alt+D"
+    
+def clear_editor(editor):
+    note = editor.note
+    # enumerate all fieldNames of the current note
+    for c, field_name in enumerate(mw.col.models.fieldNames(note.model())):
+        note[field_name] = ''
+    note.flush()  # never forget to flush
+    mw.reset()  # refresh gui
+    
+def setup_buttons(editor):
+    u"""Add the buttons to the editor."""
+    editor._addButton("clear_fields1", lambda edito=editor: clear_editor(edito), _(SHORTCUT),
+                       text=u"C", tip="Clear field entries (" + SHORTCUT +")")
+
+# register callback function that gets executed after setupEditorButtons has run. 
+# See Editor.setupEditorButtons for details
+addHook("setupEditorButtons", setup_buttons)
