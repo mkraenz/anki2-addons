0f8b4375f6a7ee652f0e989ca98adb75a91ff715
Mirco Kraenz <prosingularity@yahoo.de>
2016-04-04 18:38:13 +0200

    #2 ADD paste_to_my_field

---
+++ anki2-addons/src/paste_to_my_field.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 Mirco Kraenz <contact@kraenz.eu>
+# License: GNU GPL, version 3 or later; http://www.gnu.org/copyleft/gpl.html
+
+# Tested on Anki Version 2.0.33, Qt 4.8.4, PyQt 4.10
+
+import paste_to_my_field_by_miro.paste_to_my_field
+
+SHORTCUT = "Alt+V"
+# Adjust these settings at will. For example, specify some more FIELD_NAMES like 'Expression'
+# to paste to the 'Expression' field as well, or substitute 'Examples' by some field.
+FIELD_NAMES = ['Examples',
+               'Expression',
+               ]
---
+++ anki2-addons/src/paste_to_my_field_by_miro/paste_to_my_field.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 Mirco Kraenz <contact@kraenz.eu>
+# License: GNU GPL, version 3 or later; http://www.gnu.org/copyleft/gpl.html
+
+# Tested on Anki Version 2.0.33, Qt 4.8.4, PyQt 4.10
+
+from aqt import mw
+from aqt.editor import Editor # the editor when you click "Add" in Anki
+from anki.hooks import addHook
+from . import pyperclip
+from paste_to_my_field import FIELD_NAMES, SHORTCUT
+
+def paste_to_my_field(editor):
+    u'''Paste clipboard text to field specified by constant FIELD_NAMES '''
+    note = editor.note
+    # enumerate all fieldNames of the current note
+    for _, field_name in enumerate(mw.col.models.fieldNames(note.model())):
+        if field_name in FIELD_NAMES:
+            note[field_name] = pyperclip.paste()
+    mw.reset()
+
+def setup_button_paste_to_my_field(editor):
+    u"""Add the buttons to the editor."""
+    editor._addButton("paste_to_my_field_by_miro", 
+                      lambda edito=editor: paste_to_my_field(edito), _(SHORTCUT),
+                       text=u"P", tip="Paste to my_field (" + SHORTCUT +")")
+
+addHook("setupEditorButtons", setup_button_paste_to_my_field)
